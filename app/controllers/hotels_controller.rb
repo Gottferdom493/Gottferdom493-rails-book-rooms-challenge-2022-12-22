8f58640ab5b0c5340731e88d7027e5149e12ed29
raphael <raphael.bichet@yahoo.com>
2022-12-22 17:29:25 +0100

    ajout dans le controller de la table Hotel index, new, create, private + les views hotel index, new avec les btn clicable

--- app/controllers/hotels_controller.rb
+++ app/controllers/hotels_controller.rb
@@ -1,2 +1,28 @@
 class HotelsController < ApplicationController
+
+
+  def index
+    @hotels = Hotel.all
+  end
+
+  def new
+    @hotel = Hotel.new
+  end
+
+  def create
+    @hotel= Hotel.new(hotel_params)
+    @hotel.user = current_user #-> permet de préciser quelle est l'utilisateur qui fait la création
+    if @hotel.save
+      redirect_to hotels_path()
+    else
+      render :new
+    end
+  end
+
+private
+
+  def hotel_params
+    params.require(:hotel).permit(:name, :address)
+  end
+
 end
