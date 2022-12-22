c0cb08a7f32d4d41449947b1e7184a1e0ce948ee
raphael <raphael.bichet@yahoo.com>
2022-12-22 17:37:34 +0100

    ajout dans le controller Hotels la methode destroy + le btn dans la views index

--- app/controllers/hotels_controller.rb
+++ app/controllers/hotels_controller.rb
@@ -10,7 +10,7 @@ class HotelsController < ApplicationController
   end
 
   def create
-    @hotel= Hotel.new(hotel_params)
+    @hotel = Hotel.new(hotel_params)
     @hotel.user = current_user #-> permet de préciser quelle est l'utilisateur qui fait la création
     if @hotel.save
       redirect_to hotels_path()
@@ -19,10 +19,17 @@ class HotelsController < ApplicationController
     end
   end
 
+  def destroy
+    @hotel = Hotel.find(params[:id])
+    @hotel.destroy
+    redirect_to hotel_path(@hotel), status: :see_other
+  end
+
+
 private
 
   def hotel_params
-    params.require(:hotel).permit(:name, :address)
+    params.require(:hotel).permit(:name, :adresse)
   end
 
 end
