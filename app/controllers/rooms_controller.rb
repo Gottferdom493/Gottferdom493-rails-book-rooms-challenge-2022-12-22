c1beeb62a7cc298bd563e30f5cb26b23446af129
raphael <raphael.bichet@yahoo.com>
2022-12-22 18:11:06 +0100

    création du controller Rooms avec son index, new, create , private + les views index et new avec les btn pour y renter

---
+++ app/controllers/rooms_controller.rb
@@ -0,0 +1,32 @@
+class RoomsController < ApplicationController
+
+  def index
+    @hotel = Hotel.find(params[:hotel_id])
+    @rooms = Room.where(hotel: @hotel)
+  end
+
+  def new
+    @hotel = Hotel.find(params[:hotel_id])
+    @room = Room.new
+  end
+
+  def create
+    @room = Room.new(room_params)
+    @hotel = Hotel.find(params[:hotel_id])
+    @room.hotel = @hotel #-> le .hotel est utilisable grâce au belongs_to dans ton modèle. Cela permet de créer une méthode d'instance qui est .hotel
+    @room.user = current_user  #-> permet de préciser quelle est l'utilisateur qui fait la création
+    if @room.save
+      redirect_to hotel_rooms_path(), notice: "Comment has been added"
+    else
+      render :new
+    end
+  end
+
+
+  private
+
+  def room_params
+    params.require(:room).permit(:capacity, :price)
+  end
+
+end
