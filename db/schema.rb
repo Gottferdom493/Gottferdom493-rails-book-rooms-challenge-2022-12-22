0d74ef1497220a7e6af70d3bfbbb2afd3d695d23
raphael <raphael.bichet@yahoo.com>
2022-12-22 17:44:53 +0100

    création + migration de la table Rooms

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_22_160046) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_22_164349) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,17 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_22_160046) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_hotels_on_user_id"
+  end
+
+  create_table "rooms", force: :cascade do |t|
+    t.integer "capacity"
+    t.integer "price"
+    t.bigint "user_id", null: false
+    t.bigint "hotel_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["hotel_id"], name: "index_rooms_on_hotel_id"
+    t.index ["user_id"], name: "index_rooms_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -36,4 +47,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_22_160046) do
   end
 
   add_foreign_key "hotels", "users"
+  add_foreign_key "rooms", "hotels"
+  add_foreign_key "rooms", "users"
 end
